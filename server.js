e5f52c690a8c9bde77abc7b342558f9ae6bac8cf
Fernando Kuteken <fernando.kuteken@codus.com.br>
2019-08-15 08:38:41 -0300

    Add slides to handlebars views

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
 const path = require('path')
 const fs = require('fs')
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 const port = process.env.PORT || 3000
 
 const app = express()
 
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
 app.get('/slides', (req, res, next) => {
-  const slidesPath = path.join(__dirname, 'public', 'slides')
+  const slidesPath = path.join(__dirname, 'views', 'slides')
 
   fs.readdir(slidesPath, (err, files) => {
     if (err) {
       next(err)
     } else {
-      res.json(files)
+      res.json(files.map(file => file.replace('.handlebars', '')))
     }
   })
+})
+app.get('/slides/:slideName', (req, res) => {
+  res.render(`slides/${req.params.slideName}`, { layout: 'slide' })
 })
 app.use(express.static('public'))
 
