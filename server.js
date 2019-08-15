cdd7286e75bdd3d3dec38887cb44ade1582b6940
Fernando Kuteken <fskuteken@gmail.com>
2019-08-15 00:10:56 -0300

    Expose slides in an endpoint

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+const path = require('path')
+const fs = require('fs')
 const express = require('express')
 
 const port = process.env.PORT || 3000
 
 const app = express()
 
+app.get('/slides', (req, res, next) => {
+  const slidesPath = path.join(__dirname, 'public', 'slides')
+
+  fs.readdir(slidesPath, (err, files) => {
+    if (err) {
+      next(err)
+    } else {
+      res.json(files)
+    }
+  })
+})
 app.use(express.static('public'))
 
 app.listen(port, () => { console.log(`App listening on port ${port}`) })
